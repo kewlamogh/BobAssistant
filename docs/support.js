b4a70a0b695cb020177843979ec58e06d1519c0a
kewlamogh <amogh_sarma@outlook.com>
2021-08-13 18:25:58 -0700

    crat

---
+++ docs/support.js
@@ -0,0 +1,7 @@
+let currIndex = 0;
+let pastCommands = ["hark"];
+
+document.getElementById('cmd').addEventListener('keypress', function (event) {
+    if (event.keyCode == 13) { pastCommands.push(this.value) }
+    if (event.keyCode == 38) { currIndex += 1; if (currIndex == pastCommands.length) {currIndex = 0}; this.value = pastCommands[currIndex] }
+});
